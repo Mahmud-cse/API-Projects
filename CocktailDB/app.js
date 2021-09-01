d83f75451fb5415ab9136731a73732317615b524
Mahmud-cse <mahmudhasan555666@gmail.com>
2021-09-01 21:22:28 +0600

    api

---
+++ CocktailDB/app.js
@@ -0,0 +1,62 @@
+document.getElementById('error').style.display='none';
+
+const searchCocktail=()=>{
+    const search=document.getElementById('search');
+    const searchText=search.value;
+    search.value='';
+    document.getElementById('error').style.display='none';
+
+    if(searchText==''){
+        document.getElementById('error').style.display='block';
+    }else{
+        const url=`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchText}`;
+        fetch(url)
+        .then(res=>res.json())
+        .then(data=>displayData(data.drinks));
+    }
+}
+
+
+const displayData=(value)=>{
+    const card=document.getElementById('card');
+    card.textContent='';
+    value.forEach(data=>{
+        console.log(data);
+        const div=document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML=`
+        <div onclick=loadData(${data.idDrink}) class="card h-100">
+            <img src="${data.strDrinkThumb}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${data.strDrink}</h5>
+                <p class="card-text">${data.strInstructions.slice(0,100)}</p>
+            </div>
+        </div>
+        `;
+        card.appendChild(div)
+    })
+}
+
+const loadData=(value)=>{
+    const url=`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${value}`;
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>displaySingle(data.drinks[0]));
+}
+
+const displaySingle=(data)=>{
+    const value=document.getElementById('single');
+    value.textContent='';
+    const div=document.createElement('div');
+    div.classList.add('col');
+    div.innerHTML=`
+    <div onclick=loadData(${data.idDrink}) class="card h-100">
+    <img src="${data.strDrinkThumb}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${data.strDrink}</h5>
+            <p class="card-text">${data.strInstructions.slice(0,100)}</p>
+        </div>
+    </div>
+    `
+    value.appendChild(div);
+}
